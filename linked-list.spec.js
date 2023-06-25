bfac8ffcce0beae21353f7c2915a992c88a919c8
servicesrhino <it.services.rhino@gmail.com>
2023-06-26 00:03:54 +0300

    linked list implementation

---
+++ linked-list.spec.js
@@ -0,0 +1,17 @@
+const { LinkedList } = require('./linkedlist');
+
+function init() {
+  const list = new LinkedList();
+
+  list.append('a').append('b').append('c').append('d');
+
+  return list;
+}
+
+describe('Linked List', () => {
+  test('Append', () => {
+    let list = init();
+
+    expect(list.append('x').toString()).toBe('a,b,c,d,x');
+  });
+});
